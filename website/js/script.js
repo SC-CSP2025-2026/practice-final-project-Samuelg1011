e654b468c2f14a5e042f4ca363aefa2eb78ff5d8
Samuelg1011 <guosamuel178@gmail.com>
2026-02-19 11:01:39 -0800

    WIP: working on printing out book list

--- website/js/script.js
+++ website/js/script.js
@@ -1,21 +1,33 @@
-const url =
-  "https://student-api-proxy.onrender.com/api/open-library2.p.rapidapi.com/search_title/HarryPotter";
-const options = {
-  method: "GET",
-  headers: {
-    "X-API-Key":
-      "689953799444f4cbe8faa3c384c56a3915078b3247b70b409cb011230b7cdbe5",
-  },
+const randomButton = document.querySelector("#random-btn");
+const listGroup = document.querySelector(".list-group");
+
+const loadBooksByTitle = async (bookTitle) => {
+  const url = `https://student-api-proxy.onrender.com/api/open-library2.p.rapidapi.com/search_title/${bookTitle}&limit=1`;
+  const options = {
+    method: "GET",
+    headers: {
+      "X-API-Key":
+        "689953799444f4cbe8faa3c384c56a3915078b3247b70b409cb011230b7cdbe5",
+    },
+  };
+
+  const response = await fech(url, options);
+  const result = await response.json();
+  const data = getSampleBooks(); //result.data;
+  const books = data.books;
+  console.log(books);
+
+  listGroup.innerHTML = "";
+
+  books.forEach((book) => {
+    const listItem = `<li class="list-group-item">${book.title} - ${book.author}</li>`;
+    listGroup.insertAdjacentHTML("beforeend", listItem);
+  });
+
+  console.log(books);
 };
 
-fetch(url, options)
-  .then((response) =>
-    response.json().then((result) => {
-      console.log(result.data); // Your API data
-      console.log(`Cost: $${result.meta.cost}`);
-      console.log(`Remaining: $${result.meta.remaining_budget}`);
-    }),
-  )
-  .catch((error) => {
-    console.log(error);
-  });
+loadBooksByTitle("sample");
+
+randomButton.addEventListener("click", loadBooksByTitle);
+randomButton.books = "Harry Potter";
